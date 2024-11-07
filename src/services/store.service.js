90ab114c3fb85ad088eded3bf242a4e44c443a82
shin0112 <wnslcosltimo12@gmail.com>
2024-11-08 05:05:57 +0900

    feat: 특정 지역에 가게 추가

--- src/services/store.service.js
+++ src/services/store.service.js
@@ -6,11 +6,7 @@ import {
 export const createStore = async (data) => {
   const regionResult = await getRegionIdByRegion(data.region);
 
-  if (!regionResult.length) {
-    throw new Error("해당 지역이 존재하지 않습니다.");
-  }
-
-  const regionId = regionResult[0].id;
+  const regionId = regionResult.id;
 
   await addStore({
     name: data.name,
